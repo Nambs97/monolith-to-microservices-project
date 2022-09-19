a688d967662bf568cb9b59c219c50a7133770b13
Rarivoson Andrianambinintsoa <nambinintsoa26@yahoo.fr>
2022-09-19 17:28:44 +0300

    Change APIHOST in Frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa400fee4a4a745b88f9a2536bc003ea-1556227063.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
